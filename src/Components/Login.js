b39950dea5dda67df2b1f3583c36b4c113ef7e2e
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-18 20:39:36 +0530

    update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import "../style/login.css";
 
 export default function Login() {
+     const navigate=useNavigate();
   const login = () => {
-    localStorage.setItem("login");
+    localStorage.setItem("login",true);
+    navigate('/')
   };
+  useEffect (()=>{
+     let login = localStorage.getItem('login')
+     if(login){
+          navigate('/')
+     }
+})
   return (
     <div>
       <input type="text" name="username" id="username" />
