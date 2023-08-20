5aa8d3c9e00e16ef5c4cb18a886ddedc380fd471
Vikas Jadaun <112753171+jadaun-sahab@users.noreply.github.com>
2023-08-20 11:16:04 +0530

    update

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,28 +1,37 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../style/login.css";
 
 export default function Login() {
-     const navigate=useNavigate();
+  const navigate = useNavigate();
   const login = () => {
-    localStorage.setItem("login",true);
-    navigate('/')
+    localStorage.setItem("login", true);
+    navigate("/");
   };
-  useEffect (()=>{
-     let login = localStorage.getItem('login')
-     if(login){
-          navigate('/')
-     }
-})
+  useEffect(() => {
+    let login = localStorage.getItem("login");
+    if (login) {
+      navigate("/");
+    }
+  });
   return (
-    <div className='login'>
+    <div className="login">
       <h1>SurveyMonkey</h1>
-      <form action="/">
-
-      <input type="text" name="username" id="username" />
-      <input type="password" name="password" id="password" />
-      <button onClick={login}>Login</button>
-      </form>
+      <div className="loginContainer">
+        <h3>
+          Don’t have an account? <Link to="/">Sign up</Link>
+        </h3>
+        <h1>Log in</h1>
+        <div>
+          <input type="email" name="email" id="email" placeholder="Enter Your Email Address" />
+        </div>
+        <span>
+          <Link to="/">forget mail?</Link>
+        </span>
+        <div>
+          <button onClick={login}>Login</button>
+        </div>
+      </div>
     </div>
   );
 }
